6272fc47d2df72952254613a9e92fccde6cb0e95
Javipaur <javipaur@gmail.com>
2022-06-10 16:10:10 +0200

    primer commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { Text, View } from 'react-native'
+
+ const App = () => {
+  return (
+    <NavigationContainer>
+    <Text>AppNavigate</Text>
+    </NavigationContainer>
+  )
+}
+export default App;
